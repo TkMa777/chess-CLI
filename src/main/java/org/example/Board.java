93cdf4de544c90beee5ab522716c4f716b1a1200
Mkrtchyan Tatshat <121193072+TkMa777@users.noreply.github.com>
2023-12-03 17:22:32 +0100

    Piece visualization

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import org.example.piece.Pawn;
-import org.example.piece.Piece;
+import org.example.piece.*;
 
 import java.util.HashMap;
 
@@ -18,11 +17,53 @@ public class Board {
         pieces.put(coordinates, piece);
     }
 
+    // vérifiez les carrés vides du tableau à l'aide de la clé
+    public boolean isSquareEmpty(Coordinates coordinates) {
+        return !pieces.containsKey(coordinates);
+    }
+
+
+    // Renvoie piece aux coordinates spécifiées.
+    public Piece getPiece(Coordinates coordinates) {
+        return pieces.get(coordinates);
+    }
+
     public void setupDefaultPiecesPositions() {
+        // set pawns
         for (File file : File.values()) {
             setPieces(new Coordinates(file, 2), new Pawn(Color.WHITE, new Coordinates(file, 2)));
             setPieces(new Coordinates(file, 7), new Pawn(Color.BLACK, new Coordinates(file, 7)));
         }
+
+        // set rook
+        setPieces(new Coordinates(File.A, 1), new Rook(Color.WHITE, new Coordinates(File.A, 1)));
+        setPieces(new Coordinates(File.H, 1), new Rook(Color.WHITE, new Coordinates(File.H, 1)));
+        setPieces(new Coordinates(File.A, 8), new Rook(Color.BLACK, new Coordinates(File.H, 8)));
+        setPieces(new Coordinates(File.H, 8), new Rook(Color.BLACK, new Coordinates(File.H, 8)));
+
+
+        // set Knight
+        setPieces(new Coordinates(File.B, 1), new Knight(Color.WHITE, new Coordinates(File.B, 1)));
+        setPieces(new Coordinates(File.G, 1), new Knight(Color.WHITE, new Coordinates(File.G, 1)));
+        setPieces(new Coordinates(File.G, 8), new Knight(Color.BLACK, new Coordinates(File.G, 8)));
+        setPieces(new Coordinates(File.B, 8), new Knight(Color.BLACK, new Coordinates(File.B, 8)));
+
+
+        // set Bishop
+        setPieces(new Coordinates(File.C, 1), new Bishop(Color.WHITE, new Coordinates(File.C, 1)));
+        setPieces(new Coordinates(File.F, 1), new Bishop(Color.WHITE, new Coordinates(File.F, 1)));
+        setPieces(new Coordinates(File.C, 8), new Bishop(Color.BLACK, new Coordinates(File.C, 8)));
+        setPieces(new Coordinates(File.F, 8), new Bishop(Color.BLACK, new Coordinates(File.F, 8)));
+
+
+        // set Queen
+        setPieces(new Coordinates(File.D, 1), new Queen(Color.WHITE, new Coordinates(File.D, 1)));
+        setPieces(new Coordinates(File.D, 8), new Queen(Color.BLACK, new Coordinates(File.D, 8)));
+
+        // set King
+        setPieces(new Coordinates(File.E, 1), new King(Color.WHITE, new Coordinates(File.E, 1)));
+        setPieces(new Coordinates(File.E, 8), new King(Color.BLACK, new Coordinates(File.E, 8)));
+
     }
 
 
--- src/main/java/org/example/BoardConsoleRenderer.java
+++ src/main/java/org/example/BoardConsoleRenderer.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.piece.Piece;
+
 public class BoardConsoleRenderer {
 
     public static final String ANSI_RESET = "\u001B[0m";
@@ -13,15 +15,22 @@ public class BoardConsoleRenderer {
     // Dessine un échiquier sur CLI.
     public void render(Board board) {
         for (int rank = 8; rank >= 1 ; rank--) {
-            String line = "";
+            String line = "     ";
             for (File file : File.values()) {
-                line += getSpringForEmptySquare(new Coordinates(file, rank));
+                Coordinates coordinates = new Coordinates(file, rank);
+                if (board.isSquareEmpty(coordinates)) {
+                    line += getSpriteForEmptySquare(coordinates);
+                } else {
+                    line += getPieceSprite(board.getPiece(coordinates));
+                }
             }
 
             line += ANSI_RESET;
             System.out.println(line);
         }
     }
+
+
 
     private String colorizeSprite(String sprite, Color pieceColor, boolean isSquareDark) {
         // format = background color + font color + text
@@ -45,8 +54,27 @@ public class BoardConsoleRenderer {
 
     // Renvoie une représentation visuelle d'une case vide sur l'échiquier,
     // avec une couleur basée sur la luminosité de la case.
-    private String getSpringForEmptySquare(Coordinates coordinates) {
+    private String getSpriteForEmptySquare(Coordinates coordinates) {
         return colorizeSprite("   ", Color.WHITE, Board.isSquareDark(coordinates));
+    }
+
+    // Vérifie la méthode setupDefaultPiecesPositions, et complète board avec les pieces
+    private String getPieceSprite(Piece piece) {
+        return colorizeSprite(" " + selectUnicodeSpriteForPiece(piece) + " ", piece.color, Board.isSquareDark(piece.coordinates));
+
+    }
+
+    private String selectUnicodeSpriteForPiece(Piece piece) {
+        return switch (piece.getClass().getSimpleName()) {
+            case "Pawn" -> "♟";
+            case "Knight" -> "♞";
+            case "Bishop" -> "♝";
+            case "Rook" -> "♜";
+            case "Queen" -> "♛";
+            case "King" -> "♚";
+            default -> "";
+        };
+
     }
 
 }
